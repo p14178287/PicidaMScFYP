6e09d3e8de31be5008063a99d6dea62ffd567e63
shingiraimarikasi <shingiraimarikasi@192.168.0.11>
2018-06-11 12:47:01 +0100

    all dependencies sorted

---
+++ app/src/main/java/com/example/shingiraimarikasi/mscdmumentalhealthchecker/controller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.shingiraimarikasi.mscdmumentalhealthchecker.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shingiraimarikasi.mscdmumentalhealthchecker.R
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+//    val tag : String = "HelloShingirai"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Log.i(tag, "onCreate")
+    }
+
+
+}
