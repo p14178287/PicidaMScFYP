673465eea85c6b2c4b803c9790d37e09e5b11db9
shingiraimarikasi <shingiraimarikasi@192.168.0.11>
2018-06-12 09:44:24 +0100

    Basic layout for authentication window set

--- app/src/main/java/com/example/shingiraimarikasi/mscdmumentalhealthchecker/controller/MainActivity.kt
+++ app/src/main/java/com/example/shingiraimarikasi/mscdmumentalhealthchecker/controller/MainActivity.kt
@@ -7,12 +7,11 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-//    val tag : String = "HelloShingirai"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Log.i(tag, "onCreate")
+
     }
 
 
